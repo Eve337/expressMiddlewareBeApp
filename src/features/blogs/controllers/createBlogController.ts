0e4fe4988f89d94134ec3279f41639d0e324c2d5
Artsem Kalinin <vesunchik1945@gmail.com>
2025-04-28 16:49:08 +0300

    fix: add mapper

--- src/features/blogs/controllers/createBlogController.ts
+++ src/features/blogs/controllers/createBlogController.ts
@@ -2,6 +2,7 @@ import { Response, Request } from 'express'
 import { blogsRepository } from '../blogs.repository'
 import { BasicErrorResponse, BlogInputModel, BlogViewModel } from '../../../models'
 import { validationResult } from 'express-validator'
+import { mapToBlogViewModel } from '../../../utils/mappers'
 
 export const createBlogController = async (req: Request<any, any, BlogInputModel>, res: Response) => {
     const errors = validationResult(req)
@@ -11,7 +12,10 @@ export const createBlogController = async (req: Request<any, any, BlogInputModel
     };
     const newBlogId = await blogsRepository.create(req.body)
     const newBlog = await blogsRepository.findAndMap(String(newBlogId._id))
+    if (!newBlog) {
+        return res.status(404).json({ errorsMessages: [{ message: 'Blog not found', field: 'blogId' }] })
+    }
     return res
     .status(201)
-    .json(newBlog)
+    .json(mapToBlogViewModel(newBlog))
 }
