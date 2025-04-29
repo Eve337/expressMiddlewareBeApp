b082946a55fbcbdc49a5f1514941331eece29a01
Artsem Kalinin <vesunchik1945@gmail.com>
2025-04-29 14:58:43 +0300

    fix: fix critical errors

--- src/features/posts/middlewares/posts.middleware.ts
+++ src/features/posts/middlewares/posts.middleware.ts
@@ -14,6 +14,7 @@ export const blogIdValidator = body('blogId').isString().withMessage(() => ({ fi
     .trim().isLength({min: 1, max: 100}).withMessage(() => ({ field: 'blogId', message: 'more then 100 or 0' }))
 
 export const findBlogIdValidator = (req: Request<any, any, { blogId: string }>, res: Response, next: NextFunction) => {
+    console.log(req.body.blogId);
     const blogId = blogsRepository.findOne(req.body.blogId);
     if (!blogId) {
         res.status(404).send('Entity not found');
@@ -24,6 +25,7 @@ export const findBlogIdValidator = (req: Request<any, any, { blogId: string }>, 
 
 export const findPostValidator = async (req: Request, res: Response, next: NextFunction) => {
   const postId = await postsRepository.find(req.params.id);
+  console.log(postId);
   if (!postId) {
       res.status(404).send('Entity not found');
       return;
@@ -33,7 +35,6 @@ export const findPostValidator = async (req: Request, res: Response, next: NextF
 
 export const postValidators = [
   adminMiddleware,
-  findPostValidator,
   titleValidator,
   shortDescriptionValidator,
   contentValidator,
--- src/features/posts/posts.repository.ts
+++ src/features/posts/posts.repository.ts
@@ -1,6 +1,6 @@
 import { ObjectId, WithId } from "mongodb";
 import { PostDbType, db } from "../../db"
-import { PostInputModel, PostViewModel } from "../../models"
+import { PostInputModel } from "../../models"
 import { blogsCollection, postsCollection } from "../../utils/db";
 
 export const postsRepository = {
@@ -15,6 +15,7 @@ export const postsRepository = {
         blogName: blog?.name,
     }
     const postDbEntity = await postsCollection.insertOne(newPost);
+    console.log(postDbEntity.insertedId);
     return { ...newPost, _id: postDbEntity.insertedId };
   },
   async find(id: string) {
