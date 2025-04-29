a56898db39a20462534b52a0b092acca450208bb
Artsem Kalinin <vesunchik1945@gmail.com>
2025-04-29 15:25:29 +0300

    fix: fix critical errors

--- src/features/posts/controllers/createPostController.ts
+++ src/features/posts/controllers/createPostController.ts
@@ -2,16 +2,18 @@ import { Response, Request } from 'express'
 import { postsRepository } from '../posts.repository'
 import { BasicErrorResponse, PostInputModel, PostViewModel } from '../../../models'
 import { validationResult } from 'express-validator'
+import { mapToPostViewModel } from '../../../utils/mappers'
 
-export const createPostController = (req: Request<any, any, PostInputModel>, res: Response) => {
+export const createPostController = async (req: Request<any, any, PostInputModel>, res: Response) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         // in case request params meet the validation criteria
         return res.status(400).json({ errorsMessages: errors.array() })
     };
-    const newPost = postsRepository.create(req.body);
-
-    res.status(201)
-       .json(newPost)
-    return;
+    const newPost = await postsRepository.create(req.body);
+    if (!newPost) {
+        return res.status(400).json({ errorMessages: [{ message: 'Blog is not found', field: 'blogId' }]})
+    }
+    return res.status(201)
+       .json(mapToPostViewModel(newPost))
 }
