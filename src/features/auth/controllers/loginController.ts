549a4f7896bbd3e0738ade482220ca3b9fa9c7eb
Artsem Kalinin <vesunchik1945@gmail.com>
2025-05-21 14:00:09 +0300

    fix: remove minor bugs

--- src/features/auth/controllers/loginController.ts
+++ src/features/auth/controllers/loginController.ts
@@ -5,7 +5,7 @@ import { bcryptService } from "../../../utils/auth";
 export const loginController = async (req: Request<any, any, { loginOrEmail: string, password: string }>, res: Response) => {
     const { loginOrEmail, password } = req.body;
     const currentUser = await usersQueryRepository.findByEmailOrLogin(loginOrEmail);
-    if (!currentUser) return res.status(404).send();
+    if (!currentUser) return res.status(401).send();
     const comparePasswords = await bcryptService.checkPassword(password, currentUser.passwordHash);
     if (!comparePasswords) return res.status(401).send();
     return res.status(204).send();
--- src/features/users/service/findUserController.ts
+++ src/features/users/service/findUserController.ts
@@ -3,7 +3,7 @@ import { usersQueryRepository } from "../repository/users.query.repository";
 
 export const findUserController = async (req: Request, res: Response<any>) => {
     const searchLoginTerm = req.query.searchNameTerm ? String(req.query.searchLoginTerm) : '';
-    const searchEmailTerm = req.query.searchEmailTerm ? String(req.query.searchLoginTerm) : '';
+    const searchEmailTerm = req.query.searchEmailTerm ? String(req.query.searchEmailTerm) : '';
     const pageNumber = req.query.pageNumber ? Number(req.query.pageNumber) : 1;
     const pageSize = req.query.pageSize ? Number(req.query.pageSize) : 10;
     const sortBy = req.query.sortBy ? String(req.query.sortBy) : 'createdAt';
